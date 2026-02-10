4b453d38ff0464552e9f92a47f13d9f12719106d
Rasuljon <rasuljon@gmail.com>
2026-02-10 15:57:02 +0500

    first commit

---
+++ src/components/page.vue
@@ -0,0 +1,269 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div>
+    <div>
+      <input
+        class="bg-[#2a2927] border-[2px] border-yellow-300 rounded-[8px] h-[45px] pl-[35px] pb-[5px] ml-[589px] text-white mt-[30px] w-[550px]"
+        type="search" placeholder=" maxsulotlarni toping"> <i class="bi bi-search ml-[-540px] text-white"></i>
+      <button
+        class="bg-yellow-300 w-[103px] h-[48px] rounded-[15px] flex items-center justify-center ml-[470px] mt-[-50px]">katalog</button>
+      <img class="ml-[200px] mt-[-45px]" src="/public/sss.png" alt="">
+
+      <div class="flex items-center justify-center ml-[480px] mt-[-18px] text-[#f8f7f58a]">koloke priz</div>
+      <div class="flex items-center justify-center ml-[640px] mt-[-23px] text-[#f8f7f58a]">zakaz</div>
+      <div class="flex items-center justify-center ml-[750px] mt-[-23px] text-[#f8f7f58a]">like</div>
+      <div class="flex items-center justify-center ml-[850px] mt-[-23px] text-[#f8f7f58a]">savat</div>
+      <div class="flex items-center justify-center ml-[1000px] mt-[-23px] text-[#f8f7f58a]">market all</div>
+      <button
+        class="bg-[#f8f7f51f] w-[76px] h-[44px] rounded-[15px] flex items-center justify-center ml-[1510px] mt-[-40px] text-white">kirish</button>
+    </div>
+    <i class="bi bi-box2-fill flex items-center justify-center ml-[640px] mt-[-53px] text-[#f8f7f58a] text-[18px]"></i>
+    <i class="bi bi-suit-heart flex items-center justify-center ml-[749px] mt-[-15px] text-[#f8f7f58a] text-[18px]"></i>
+    <i class="bi bi-basket3 flex items-center justify-center ml-[849px] mt-[-18px] text-[#f8f7f58a] text-[18px]"></i>
+    <i
+      class="bi bi-chat-heart flex items-center justify-center ml-[1000px] mt-[-18px] text-[#f8f7f58a] text-[18px]"></i>
+
+    <div class="hygJ1 _2K7tt flex items-center justify-center ml-[480px] mt-[-40px] text-[#f8f7f58a] text-[18px] "><span
+        data-auto="headerFortuneWheelBadgeData"
+        class="ds-text ds-text_group_core ds-text_weight_bold ds-text_color_text-invert-primary ds-text_proportional ds-text_typography_lead-text ds-text_lead-text_tight ds-text_lead-text_bold">Призы</span><svg
+        width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="_1C9hR">
+        <g>
+          <mask id="b" maskUnits="userSpaceOnUse" x="-2" y="-2" width="20" height="20">
+            <path d="M13.1109-1.56506-1.14816 2.17572 2.88915 17.565l14.25905-3.7407-4.0373-15.38936Z" fill="#fff">
+            </path>
+          </mask>
+          <g mask="url(#b)">
+            <path
+              d="M15.0225 5.87169c.9691 4.52331-1.3392 8.82791-5.3452 9.87881-3.83154.8769-7.58498-1.7603-8.744342-6.1796C.470435 5.2577 2.47282 1.52082 6.01492.44859c4.16728-.44983112 7.95648.51655 9.00758 5.4231Z"
+              fill="#A45C27"></path>
+            <path
+              d="M7.57638 13.6414c2.05933.1259 3.87782-2.2067 4.06152-5.21025.1838-3.00356-1.3367-5.54056-3.39604-5.66654-2.05932-.126-3.87772 2.20671-4.06148 5.21024-.18376 3.00355 1.33668 5.54045 3.396 5.66655Z"
+              fill="#D48A40"></path>
+            <path d="M10.1613 8.27186 7.51712 12.3275 5.30939 8.47703 7.66918 3.8324l2.49212 4.43946Z" fill="url(#c)">
+            </path>
+            <path fill-rule="evenodd" clip-rule="evenodd"
+              d="M6.6317 15.7507c3.4828.2131 6.5198-3.1041 6.7831-7.40909.2635-4.30507-2.3464-7.967733-5.82932-8.180799C4.10263-.0522895 1.06568 3.26488.802299 7.56991.538931 11.875 3.14883 15.5377 6.6317 15.7507Zm.0288-2.2117c2.50338.1532 4.6862-2.2311 4.8756-5.32545.1893-3.0944-1.68668-5.727-4.19007-5.88018-2.50337-.15315-4.68624 2.23113-4.87556 5.32547C2.28116 10.7532 4.15709 13.3859 6.6605 13.539Z"
+              fill="#EFD35A"></path>
+            <path d="m6.77257 7.97491.71108 4.29119-2.15694-3.76454 1.44586-.52665Z" fill="url(#d)"></path>
+            <path d="m6.78097 7.92358.89311-4.00143-2.37946 4.55454 1.48635-.55311Z" fill="url(#e)"></path>
+            <path d="m6.79262 7.94916.74627 4.35564L10.151 8.27728l-3.35838-.32812Z" fill="#E4BF46"></path>
+            <path d="m7.68302 3.86896-.8904 4.04978 3.24038.27385-2.34998-4.32363Z" fill="url(#f)"></path>
+            <path fill-rule="evenodd" clip-rule="evenodd"
+              d="m6.77373 7.97344-1.44985.52824-.02391-.0267 1.48453-.55034.84868-4.05129.08781.01937-.88664 4.01887 3.30755.32548.0194.0352-.0113.01772-3.32125-.33101.73335 4.30352-.04615.0671-.03249-.0645-.70973-4.29166Z"
+              fill="#FDE57F"></path>
+          </g>
+        </g>
+        <defs>
+          <linearGradient id="c" x1="6.86543" y1="4.277" x2="7.58075" y2="12.2983" gradientUnits="userSpaceOnUse">
+            <stop offset=".401938" stop-color="#E9C44C"></stop>
+            <stop offset=".505928" stop-color="#E2C243"></stop>
+            <stop offset=".639417" stop-color="#F0D155"></stop>
+          </linearGradient>
+          <linearGradient id="d" x1="6.73829" y1="7.98741" x2="6.44137" y2="10.6613" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#E6C049"></stop>
+            <stop offset="1" stop-color="#EFD054"></stop>
+          </linearGradient>
+          <linearGradient id="e" x1="5.69281" y1="7.70093" x2="6.97876" y2="7.42819" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#F2D659"></stop>
+            <stop offset="1" stop-color="#E8C34B"></stop>
+          </linearGradient>
+          <linearGradient id="f" x1="8.24339" y1="5.68137" x2="7.94197" y2="9.02446" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#EFD35A"></stop>
+            <stop offset="1" stop-color="#DEB93C"></stop>
+          </linearGradient>
+        </defs>
+      </svg></div>
+    <div class="ml-[200px] mt-[50px]">
+      <hover class="text-yellow-400">
+        <a class="text-white m-[15px] w-[180px] h-[80px] bg-blue-600 rounded-[10px]" href="#">Все хиты</a>
+        <a class="text-white m-[15px]" href="#">Ultima</a>
+        <a class="text-white m-[15px]" href="#">Любимая категория</a>
+        <a class="text-white m-[15px]" href="#">из-за рубежа</a>
+        <a class="text-white m-[15px]" href="#">Одежда</a>
+        <a class="text-white m-[15px]" href="#">Дом</a>
+        <a class="text-white m-[15px]" href="#">Ремонт</a>
+        <a class="text-white m-[15px]" href="#">Детям</a>
+        <a class="text-white m-[15px]" href="#">Красота</a>
+        <a class="text-white m-[15px]" href="#">Покупайте как юрлицо</a>
+        <a class="text-white m-[15px]" href="#">Покупайте как </a>
+        <a class="text-white m-[15px]" href="#">моска укажите адрес</a>
+      </hover>
+    </div>
+    <h1 class="text-[#f8f7f58a] ml-[210px] mt-[20px]">Одежда, обувь и аксессуары•Обувь для взрослых•Туфли, мокасины,
+      мюли для взрослых•Туфли для взрослых•Туфли для взрослых BRANKI</h1>
+    <img class="ml-[215px] mt-[30px] rounded-[10px] w-[99px] h-[133px]" src="https://avatars.mds.yandex.net/get-mpic/5235182/2a0000019309d898478fcb111906ee859f1b/180x240" alt="">
+    <img class="ml-[215px] mt-[20px] rounded-[10px] w-[99px] h-[133px]"
+      src="https://avatars.mds.yandex.net/get-mpic/6453326/2a0000019309d8cec5f87825489487b08f68/180x240" alt="">
+    <img class="ml-[215px] mt-[20px] rounded-[10px] w-[99px] h-[133px]"
+      src="https://avatars.mds.yandex.net/get-mpic/5245589/2a0000019309d8bed97a4ffc2853447d4278/180x240" alt="">
+    <img class="ml-[215px] mt-[20px] rounded-[10px] w-[99px] h-[133px]"
+      src="https://avatars.mds.yandex.net/get-mpic/5243609/2a0000019309d8974d73b98313314db467f2/180x240" alt="">
+
+      <img class="ml-[350px] mt-[-590px] rounded-[10px] w-[480px] h-[586px]"
+      src="https://avatars.mds.yandex.net/get-mpic/5235182/2a0000019309d898478fcb111906ee859f1b/180x240" alt="">
+      <button class="bg-[#ff5226] w-[136px] h-[48px] rounded-[5px] flex items-center justify-center ml-[880px] mt-[-590px] text-white">СКИДКИ НЕДЕЛИ</button> 
+      <div class="w-[300px] h-[30px] bg-[#3d3c3e] ml-[880px] mt-[20px] rounded-[10px] text-white">
+<p class="ml-[200px] mb-[-25px]">Намекнуть</p>
+<p class="mt-[-20px] ml-[10px]">Хочу это в подарок </p>
+      </div>
+      <h1 class="text-white ml-[880px] mt-[20px] text-[20px]">BRANKI <i class="bi bi-chevron-compact-right"></i>Туфли BRANKI</h1>
+      <h1 class="text-white ml-[880px] mt-[10px] text-[13px]">⭐⭐⭐⭐⭐4.8(48) · 157 купили</h1>
+      <h1 class="text-white ml-[880px] mt-[10px] text-[13px]">
+        Размер·RU/Бренда
+      </h1>
+      <button class="text-white border w-[40px] h-[40px] rounded-[10px] ml-[880px] mt-[10px]">40</button>
+      <button class="text-white border w-[40px] h-[40px] rounded-[10px] ml-[15px] mt-[10px]">41</button>
+      <button class="text-white border w-[40px] h-[40px] rounded-[10px] ml-[15px] mt-[10px]">42</button>
+      <button class="text-white border w-[40px] h-[40px] rounded-[10px] ml-[15px] mt-[10px]">43</button>
+      <button class="text-white border w-[40px] h-[40px] rounded-[10px] ml-[15px] mt-[10px]">44</button>
+      <button class="text-white border w-[40px] h-[40px] rounded-[10px] ml-[15px] mt-[10px]">45</button>
+      <h1 class="text-[#f8f7f58a]  ml-[880px] mt-[10px]">Таблица размеров <i class="bi bi-chevron-compact-right"></i></h1>
+      <h1 class="text-[#f8f7f58a] ml-[880px] mt-[10px]  ">Артикул Маркета </h1>
+      <div class="border-t-2 ml-[1000px] w-[100px]  "></div>
+      <h1 class="text-white ml-[1100px] mt-[-25px]"> 4598449243</h1>
+       <h1 class="text-[#f8f7f58a] ml-[880px] mt-[10px]  ">Цвет товара </h1>
+      <div class="border-t-2 ml-[950px] w-[150px]  "></div>
+      <h1 class="text-white ml-[1100px] mt-[-25px]"> черный</h1>
+       <h1 class="text-[#f8f7f58a] ml-[880px] mt-[10px]  ">Материал верха </h1>
+      <div class="border-t-2 ml-[1000px] w-[100px]  "></div>
+      <h1 class="text-blue-600 ml-[1100px] mt-[-25px]"> искусственная</h1>
+         <h1 class="text-[#f8f7f58a] ml-[880px] mt-[10px]  ">Материал подошвы </h1>
+      <div class="border-t-2 ml-[1000px] w-[100px]  "></div>
+      <h1 class="text-blue-600 ml-[1100px] mt-[-25px]"> 8 (RU)/ на широ </h1>
+         <h1 class="text-[#f8f7f58a] ml-[880px] mt-[10px]  ">Материал верха </h1>
+      <div class="border-t-2 ml-[1000px] w-[100px]  "></div>
+      <h1 class="text-blue-600 ml-[1100px] mt-[-25px]"> искусственная </h1>
+      <h1 class="text-white ml-[880px] mt-[10px]">Все характеристики <i class="bi bi-chevron-compact-right"></i></h1>
+
+     <div class="mt-[-490px]">
+       <a class="text-white m-[20px] ml-[1300px] " href="#">Сравнить</a>
+        <a class="text-white m-[15px]" href="#">В избранное </a>
+        <a class="text-white m-[15px]" href="#">Поделиться</a>
+     </div>
+     <div class="w-[360px] h-[123px] bg-[#3d3c3a] ml-[1280px] rounded-[15px] mt-[20px]">
+       <h1 class="text-white ml-[20px] mt-[20px] text-[20px]"><b class="text-green-600">2 038 ₽  Пэй </b><b class="text-[#f8f7f58a]">4 900 ₽ –58%</b></h1>
+       <button class="bg-[#f8f7f58a] text-[black] w-[136px] h-[48px] rounded-[15px] flex items-center justify-center ml-[20px] mt-[25px] text-white">КУПИТЬ СИЧАС</button>
+       <button class="bg-yellow-400 text-black w-[136px] h-[48px] rounded-[15px] flex items-center justify-center ml-[200px] mt-[-50px] text-white">В корзину</button>
+     </div>
+     <div class="w-[360px] h-[52px] bg-[#3d3c3a]  ml-[1280px] rounded-[15px] mt-[20px]">
+      <div class="w-[50px] h-[50px] bg-red-600 rounded-full"></div>
+      <h1 class="mt-[-50px] ml-[55px] text-white">Войдите, и станет дешевле</h1>
+      <h1 class=" ml-[55px] text-[#f8f7f58a]">В аккаунте больше скидок</h1>
+     </div>
+      <button class="ml-[1579px] mt-[-40px] text-white border rounded-[10px] w-[50px] h-[30px] flex items-center justify-center">войти</button>
+      <div class="w-[360px] h-[229px] bg-[#3d3c3a]  ml-[1280px] rounded-[15px] mt-[20px]">
+        <h1 class="text-white text-[20px] ml-[30px] mt-[20px]">Доставка Маркета</h1>
+        <h1 class="text-[#f7f6f5] ml-[30px] ">Завтра, <b class="text-[#f8f7f58a]">по клику  0 ₽</b> <br><b class="text-[#f8f7f58a]">Москва</b> <br><b class="text-[#f8f7f58a]">15-30 мин. курьером в удобный момент</b>
+        Завтра, <b class="text-[#f8f7f58a]">пункт выдачи  0 ₽</b> <br><b class="text-[#f8f7f58a]">Москва</b> <br><b class="text-[#f8f7f58a]">Есть примерка</b></h1>
+      <div class="border-t-2  w-[360px]  border-[#504f4d]"></div>
+        <i class="bi bi-geo-alt text-white ml-[10px] text-[20px] mt-[20px]"></i>
+        <i class="bi bi-chevron-right text-white ml-[300px] text-[20px] mt-[30px]"></i>
+      <h1 class="text-[#f7f6f5] ml-[50px] mt-[-30px]">Укажите адрес</h1>
+      <h1 class="text-white ml-[50px]">Уточним дату и стоимость доставки</h1>
+      </div>
+      <div class="w-[360px] h-[52px] bg-[#3d3c3a]  ml-[1280px] rounded-[15px] mt-[20px]">
+      <div class="w-[50px] h-[50px] bg-white rounded-full"> <img src="https://avatars.mds.yandex.net/get-marketcms/1779479/img-9c044a62-4bc5-4d2c-9533-bc25b74c2d8f.png/favicon_48x48" alt=""></div>
+      <h1 class="mt-[-50px] ml-[55px] text-white"> BRANKI</h1>
+      <h1 class=" ml-[55px] text-[#f8f7f58a]">Магазин•⭐4.75.6K оценок</h1>
+      
+     </div>
+     <img class="w-[360px] h-[200px] rounded-[20px] ml-[1280px] mt-[20px]" src="https://storage.mds.yandex.net/get-bstor/16142106/c3f9c36b-4046-490b-98f4-4ed57332a818.gif" alt="">
+
+        <div class="">
+          <img class="ml-[215px] mt-[40px] rounded-[10px]" src="/public/tort.png" alt="">
+          <p class="text-white ml-[220px] mt-[15px]">kamzir<br><b class="text-[#0BB35C]">2 037₽Пэй </b></p>
+         </div>
+         <div class="mt-[-392px] ml-[230px]">
+          <img class="ml-[215px] mt-[40px] rounded-[10px]" src="/public/ikki.png" alt="">
+          <p class="text-white ml-[220px] mt-[15px]">qurtka <br><b class="text-[#0BB35C]">2 037₽Пэй </b></p>
+         </div>
+           <div class="mt-[-392px] ml-[460px]">
+          <img class="ml-[215px] mt-[40px] rounded-[10px]" src="/public/uch.png" alt="">
+          <p class="text-white ml-[220px] mt-[15px]">kamzir <br><b class="text-[#0BB35C]">2 037₽Пэй </b></p>
+         </div>
+          <div class="mt-[-392px] ml-[690px]">
+          <img class="ml-[215px] mt-[40px] rounded-[10px]" src="/public/tort.png" alt="">
+          <p class="text-white ml-[220px] mt-[15px]">oyoq kiyim <br><b class="text-[#0BB35C]">2 037₽Пэй </b></p>
+         </div>
+          <div class="mt-[-392px] ml-[920px]">
+          <img class="ml-[215px] mt-[40px] rounded-[10px]" src="/public/ikki.png" alt="">
+          <p class="text-white ml-[220px] mt-[15px]">kastim <br><b class="text-[#0BB35C]">2 037₽Пэй </b></p>
+         </div>
+         <div class="mt-[-392px] ml-[1150px]">
+          <img class="ml-[215px] mt-[40px] rounded-[10px]" src="/public/ikki.png" alt="">
+          <p class="text-white ml-[220px] mt-[15px]">kastim <br><b class="text-[#0BB35C]">2 037₽Пэй </b></p>
+         </div>
+
+              <div class="w-[1897px] h-[715px]"> 
+    <div class="w-[1897px] border-b-2 border-[#3f3e3c] mt-[40px]"></div>
+    <p class="text-white ml-[220px] mt-[15px] text-[25px]">Покупайте в приложении, <br>так ещё удобнее </p>  
+      <img class="ml-[210px] mt-[30px]" src="/public/jj.png" alt="">
+      <img class="ml-[210px] mt-[50px]" src="/public/ll.png" alt="">
+        <p class="ml-[210px] mt-[10px] text-white"> Наведите камеру на QR-код, <br>
+чтобы скачать приложение</p>
+          <p class="m-[10px] mt-[200px] text-white"> Статистика 
+Пользовательское соглашение</p>
+           <p class="text-white ml-[600px] mt-[-650px]"> Категории</p>
+           <p class="ml-[600px] text-[#f8f7f58a]"> 
+Досуг и развлечения <br>
+Мебель  <br>
+Бытовая техника <br>
+Гигиена <br>
+Электроника <br>
+Одежда и обувь <br>
+Товары для дома <br>
+Аптека <br>
+Красота и уход <br>
+Хобби и творчество <br>
+Дача и сад <br>
+Книги <br>
+Товары для школы и офиса <br>
+Спортивные товары <br>
+Товары для животных <br>
+Строительство и ремонт <br>
+Детские товары <br>
+Ювелирные украшения</p>
+
+
+      <p class="text-white ml-[900px] mt-[-455px]"> Покупателям</p>
+           <p class="ml-[900px] text-[#f8f7f58a]"> 
+
+Как выбрать товар <br>
+Оплата и доставка <br>
+Покупайте как юрлицо <br>
+О сервисе <br>
+Возвраты <br>
+ <br>
+ <br>
+<b class="text-[#ffff]">Продавцам </b> <br>
+Красота и уход <br>
+Хобби и творчество <br>
+Дача и сад <br>
+Книги <br>
+Товары для школы и офиса <br>
+Спортивные товары <br>
+</p>
+
+   <p class="text-white ml-[1200px] mt-[-360px]">Сотрудничество </p>
+           <p class="ml-[1200px] text-[#f8f7f58a]"> 
+
+Новости компании  <br>
+Реферальная программа  <br>
+Производителям  <br>
+Пункт выдачи заказов  <br>
+Маркет нанимает<br>
+ <br>
+ <br>
+ <br>
+<b class="text-[#ffff]">Мы в соцсетях </b> <br>
+Вконтакте <br>
+</p>
+<p class="text-white ml-[1600px] mt-[330px]"> © 2026  ООО «ЯНДЕКС МАРКЕТ» <br>
+Проект компании
+</p>
+      </div>
+      </div>
+</template> 
---
+++ src/components/pages.vue
@@ -0,0 +1,168 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div>
+    <div>
+      <input
+        class="bg-[#2a2927] border-[2px] border-yellow-300 rounded-[8px] h-[45px] pl-[35px] pb-[5px] ml-[589px] text-white mt-[30px] w-[550px]"
+        type="search" placeholder=" maxsulotlarni toping"> <i class="bi bi-search ml-[-540px] text-white"></i>
+      <button
+        class="bg-yellow-300 w-[103px] h-[48px] rounded-[15px] flex items-center justify-center ml-[470px] mt-[-50px]">katalog</button>
+      <img class="ml-[200px] mt-[-45px]" src="/public/sss.png" alt="">
+
+      <div class="flex items-center justify-center ml-[480px] mt-[-18px] text-[#f8f7f58a]">koloke priz</div>
+      <div class="flex items-center justify-center ml-[640px] mt-[-23px] text-[#f8f7f58a]">zakaz</div>
+      <div class="flex items-center justify-center ml-[750px] mt-[-23px] text-[#f8f7f58a]">like</div>
+      <div class="flex items-center justify-center ml-[850px] mt-[-23px] text-[#f8f7f58a]">savat</div>
+      <div class="flex items-center justify-center ml-[1000px] mt-[-23px] text-[#f8f7f58a]">market all</div>
+      <button
+        class="bg-[#f8f7f51f] w-[76px] h-[44px] rounded-[15px] flex items-center justify-center ml-[1510px] mt-[-40px] text-white">kirish</button>
+    </div>
+    <i class="bi bi-box2-fill flex items-center justify-center ml-[640px] mt-[-53px] text-[#f8f7f58a] text-[18px]"></i>
+    <i class="bi bi-suit-heart flex items-center justify-center ml-[749px] mt-[-15px] text-[#f8f7f58a] text-[18px]"></i>
+    <i class="bi bi-basket3 flex items-center justify-center ml-[849px] mt-[-18px] text-[#f8f7f58a] text-[18px]"></i>
+    <i
+      class="bi bi-chat-heart flex items-center justify-center ml-[1000px] mt-[-18px] text-[#f8f7f58a] text-[18px]"></i>
+
+    <div class="hygJ1 _2K7tt flex items-center justify-center ml-[480px] mt-[-40px] text-[#f8f7f58a] text-[18px] "><span
+        data-auto="headerFortuneWheelBadgeData"
+        class="ds-text ds-text_group_core ds-text_weight_bold ds-text_color_text-invert-primary ds-text_proportional ds-text_typography_lead-text ds-text_lead-text_tight ds-text_lead-text_bold">Призы</span><svg
+        width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="_1C9hR">
+        <g>
+          <mask id="b" maskUnits="userSpaceOnUse" x="-2" y="-2" width="20" height="20">
+            <path d="M13.1109-1.56506-1.14816 2.17572 2.88915 17.565l14.25905-3.7407-4.0373-15.38936Z" fill="#fff">
+            </path>
+          </mask>
+          <g mask="url(#b)">
+            <path
+              d="M15.0225 5.87169c.9691 4.52331-1.3392 8.82791-5.3452 9.87881-3.83154.8769-7.58498-1.7603-8.744342-6.1796C.470435 5.2577 2.47282 1.52082 6.01492.44859c4.16728-.44983112 7.95648.51655 9.00758 5.4231Z"
+              fill="#A45C27"></path>
+            <path
+              d="M7.57638 13.6414c2.05933.1259 3.87782-2.2067 4.06152-5.21025.1838-3.00356-1.3367-5.54056-3.39604-5.66654-2.05932-.126-3.87772 2.20671-4.06148 5.21024-.18376 3.00355 1.33668 5.54045 3.396 5.66655Z"
+              fill="#D48A40"></path>
+            <path d="M10.1613 8.27186 7.51712 12.3275 5.30939 8.47703 7.66918 3.8324l2.49212 4.43946Z" fill="url(#c)">
+            </path>
+            <path fill-rule="evenodd" clip-rule="evenodd"
+              d="M6.6317 15.7507c3.4828.2131 6.5198-3.1041 6.7831-7.40909.2635-4.30507-2.3464-7.967733-5.82932-8.180799C4.10263-.0522895 1.06568 3.26488.802299 7.56991.538931 11.875 3.14883 15.5377 6.6317 15.7507Zm.0288-2.2117c2.50338.1532 4.6862-2.2311 4.8756-5.32545.1893-3.0944-1.68668-5.727-4.19007-5.88018-2.50337-.15315-4.68624 2.23113-4.87556 5.32547C2.28116 10.7532 4.15709 13.3859 6.6605 13.539Z"
+              fill="#EFD35A"></path>
+            <path d="m6.77257 7.97491.71108 4.29119-2.15694-3.76454 1.44586-.52665Z" fill="url(#d)"></path>
+            <path d="m6.78097 7.92358.89311-4.00143-2.37946 4.55454 1.48635-.55311Z" fill="url(#e)"></path>
+            <path d="m6.79262 7.94916.74627 4.35564L10.151 8.27728l-3.35838-.32812Z" fill="#E4BF46"></path>
+            <path d="m7.68302 3.86896-.8904 4.04978 3.24038.27385-2.34998-4.32363Z" fill="url(#f)"></path>
+            <path fill-rule="evenodd" clip-rule="evenodd"
+              d="m6.77373 7.97344-1.44985.52824-.02391-.0267 1.48453-.55034.84868-4.05129.08781.01937-.88664 4.01887 3.30755.32548.0194.0352-.0113.01772-3.32125-.33101.73335 4.30352-.04615.0671-.03249-.0645-.70973-4.29166Z"
+              fill="#FDE57F"></path>
+          </g>
+        </g>
+        <defs>
+          <linearGradient id="c" x1="6.86543" y1="4.277" x2="7.58075" y2="12.2983" gradientUnits="userSpaceOnUse">
+            <stop offset=".401938" stop-color="#E9C44C"></stop>
+            <stop offset=".505928" stop-color="#E2C243"></stop>
+            <stop offset=".639417" stop-color="#F0D155"></stop>
+          </linearGradient>
+          <linearGradient id="d" x1="6.73829" y1="7.98741" x2="6.44137" y2="10.6613" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#E6C049"></stop>
+            <stop offset="1" stop-color="#EFD054"></stop>
+          </linearGradient>
+          <linearGradient id="e" x1="5.69281" y1="7.70093" x2="6.97876" y2="7.42819" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#F2D659"></stop>
+            <stop offset="1" stop-color="#E8C34B"></stop>
+          </linearGradient>
+          <linearGradient id="f" x1="8.24339" y1="5.68137" x2="7.94197" y2="9.02446" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#EFD35A"></stop>
+            <stop offset="1" stop-color="#DEB93C"></stop>
+          </linearGradient>
+        </defs>
+      </svg></div>
+    <div class="ml-[200px] mt-[50px]">
+      <hover class="text-yellow-400">
+        <a class="text-white m-[15px] w-[180px] h-[80px] bg-blue-600 rounded-[10px]" href="#">Все хиты</a>
+        <a class="text-white m-[15px]" href="#">Ultima</a>
+        <a class="text-white m-[15px]" href="#">Любимая категория</a>
+        <a class="text-white m-[15px]" href="#">из-за рубежа</a>
+        <a class="text-white m-[15px]" href="#">Одежда</a>
+        <a class="text-white m-[15px]" href="#">Дом</a>
+        <a class="text-white m-[15px]" href="#">Ремонт</a>
+        <a class="text-white m-[15px]" href="#">Детям</a>
+        <a class="text-white m-[15px]" href="#">Красота</a>
+        <a class="text-white m-[15px]" href="#">Покупайте как юрлицо</a>
+        <a class="text-white m-[15px]" href="#">Покупайте как </a>
+        <a class="text-white m-[15px]" href="#">моска укажите адрес</a>
+      </hover>
+    </div>
+    <h1 class="text-[32px] flex items-center justify-center mt-[300px] ml-[-30px] text-white">Войдите в аккаунт</h1>
+    <h1 class="text-white flex items-center justify-center  ml-[-30px]">И тогда сможете посмотреть <br>свои заказы, если они были</h1>
+    <button class="bg-yellow-300 w-[103px] h-[48px] rounded-[15px] flex items-center justify-center ml-[880px] mt-[30px]">Войти</button>
+
+    <div class="w-[1897px] h-[715px]"> 
+    <div class="w-[1897px] border-b-2 border-[#3f3e3c] mt-[40px]"></div>
+    <p class="text-white ml-[220px] mt-[15px] text-[25px]">Покупайте в приложении, <br>так ещё удобнее </p>  
+      <img class="ml-[210px] mt-[30px]" src="/public/jj.png" alt="">
+      <img class="ml-[210px] mt-[50px]" src="/public/ll.png" alt="">
+        <p class="ml-[210px] mt-[10px] text-white"> Наведите камеру на QR-код, <br>
+чтобы скачать приложение</p>
+          <p class="m-[10px] mt-[200px] text-white"> Статистика 
+Пользовательское соглашение</p>
+           <p class="text-white ml-[600px] mt-[-650px]"> Категории</p>
+           <p class="ml-[600px] text-[#f8f7f58a]"> 
+Досуг и развлечения <br>
+Мебель  <br>
+Бытовая техника <br>
+Гигиена <br>
+Электроника <br>
+Одежда и обувь <br>
+Товары для дома <br>
+Аптека <br>
+Красота и уход <br>
+Хобби и творчество <br>
+Дача и сад <br>
+Книги <br>
+Товары для школы и офиса <br>
+Спортивные товары <br>
+Товары для животных <br>
+Строительство и ремонт <br>
+Детские товары <br>
+Ювелирные украшения</p>
+
+
+      <p class="text-white ml-[900px] mt-[-455px]"> Покупателям</p>
+           <p class="ml-[900px] text-[#f8f7f58a]"> 
+
+Как выбрать товар <br>
+Оплата и доставка <br>
+Покупайте как юрлицо <br>
+О сервисе <br>
+Возвраты <br>
+ <br>
+ <br>
+<b class="text-[#ffff]">Продавцам </b> <br>
+Красота и уход <br>
+Хобби и творчество <br>
+Дача и сад <br>
+Книги <br>
+Товары для школы и офиса <br>
+Спортивные товары <br>
+</p>
+
+   <p class="text-white ml-[1200px] mt-[-360px]">Сотрудничество </p>
+           <p class="ml-[1200px] text-[#f8f7f58a]"> 
+
+Новости компании  <br>
+Реферальная программа  <br>
+Производителям  <br>
+Пункт выдачи заказов  <br>
+Маркет нанимает<br>
+ <br>
+ <br>
+ <br>
+<b class="text-[#ffff]">Мы в соцсетях </b> <br>
+Вконтакте <br>
+</p>
+<p class="text-white ml-[1600px] mt-[330px]"> © 2026  ООО «ЯНДЕКС МАРКЕТ» <br>
+Проект компании
+</p>
+      </div>
+      </div>
+</template> 
